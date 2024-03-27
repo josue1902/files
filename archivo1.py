e8dcccdca373b3895ec5f136538244e3696a65da
Josue <Josueelizondo307@gmail.com>
2024-03-26 23:22:18 -0300

    resultados

--- archivo1.py
+++ archivo1.py
@@ -1,3 +1,5 @@
+import os
+import sys
 """
 Ejercicio: Manejo de archivos con excepciones
 
@@ -15,22 +17,41 @@ Salir del programa.
 
 """
 
-class Archivos:
-    def __init__(self, crear, leer, sobrescribir, eliminar, salir):
-        self.crear = crear
-        self.leer = leer
-        self.sobrescribir = sobrescribir
-        self.eliminar = eliminar
-        self.salir = salir
 
-    def crear_archivo(self,nombre, tipo):
-        file = open(nombre, tipo)
-        print("se ha creado el archivo correctamen")
+def crear_archivo(nombre, tipo_lectura):
+    try:
+        file = open(nombre, tipo_lectura)
+        print("El archivo se ha creado correctamente")
+        return file
+    
 
+    except FileExistsError:
+        print("El archivo ya existe en el directorio")
+
+
+def escribir_archivo(nombre_archivo, tipo_lectura, texto):
+    file = nombre_archivo
+    if os.path.exists(file):
+        with open (nombre_archivo, tipo_lectura) as file:
+            file.write(texto)
+
+
+def eliminar_archivo(archivo_a_eliminar):
+    try:
+        eliminar = archivo_a_eliminar
+        if os.path.exists(eliminar):
+            os.remove(eliminar)
+            print("el archivo se ha eliminado")
+        else:
+            raise FileNotFoundError("El archivo que queres eliminar no existe")
         
+    except FileNotFoundError as error:
+        print(error)
+        
+    except FileExistsError:
+        print("El archivo que queres eliminar no existe")
 
 
-archivo_prueba = Archivos()
-
-archivo_prueba.crear("prueba.txt", "w")
-
+#res = crear_archivo("texto.txt", "x")
+#resultado = eliminar_archivo("texto.txt")
+resultado = escribir_archivo("texto.txt", "w", "estoy escribiendo un archivo")
--- teoria/conceptos.md
+++ teoria/conceptos.md
@@ -1,1 +1,14 @@
 ### Manejo de archivos
+
+**r(lectura)** : se puede leer el archivo pero no escribir ni modificarlo, si el archivo no existe, nos tirara un error.
+
+**w(escritura)** : podes esccibir el archivo, en caso de que el archivo no exista, nos creara uno nuevo.
+
+**x**: para crear un archivo si no existe usamos el modo x, si el archivo ya existe no tirara un error.
+
+tenemos varios metodos que podemos usar, pueden estar las siguientes:
+
+**read()**: este metodo sirve para todo el contenido de un archivo
+
+**readline()**: lee el archivo linea por linea
+
